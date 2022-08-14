f4da0328b71e051c7c77b95d93c6a4a9909cefc7
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-14 06:01:05 +0600

    onclik different option teaching

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('test');
+//Option 1
+//Drictly Set on html element.
+//Add option 2 onlcik function
+function makeRed(){
+            document.body.style.backgroundColor = 'red';
+        }
+//Option 3 
+const makeBlueButton = document.getElementById('make-blue');
+        makeBlueButton.onclick = makeBlue;
+
+        function makeBlue(){
+            document.body.style.backgroundColor = 'blue';
+        }
+//Option 3- Another 
+const purpleButton = document.getElementById('make-pruple');
+        purpleButton.onclick = function makePurple(){
+            document.body.style.backgroundColor = 'purple'
+        }
