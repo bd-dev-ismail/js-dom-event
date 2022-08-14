b7cdc6ace376b3aed27e50920b9cd04b13159297
bd-dev-ismail <ismailratul7@gmail.com>
2022-08-14 08:08:36 +0600

    event listener

--- js/script.js
+++ js/script.js
@@ -11,8 +11,24 @@ const makeBlueButton = document.getElementById('make-blue');
         function makeBlue(){
             document.body.style.backgroundColor = 'blue';
         }
-//Option 3- Another 
+//Option 3- Another [Use sometime]
 const purpleButton = document.getElementById('make-pruple');
         purpleButton.onclick = function makePurple(){
             document.body.style.backgroundColor = 'purple'
         }
+//Option -4-- <button id="make-pink">Make Pinik</button>
+const makePinkButton = document.getElementById('make-pink');
+        makePinkButton.addEventListener('click', makePink);
+        function makePink(){
+            document.body.style.backgroundColor = 'pink';
+        }
+
+//Option4.5 <button id="make-green">Make Green</button>
+ const makeGreenButton = document.getElementById('make-green');
+        makeGreenButton.addEventListener('click', function makeGreen(){
+            document.body.style.backgroundColor = 'green';
+        })
+//Option -4 Final Version----<button id="make-goldenrod">Make goldenRod</button>
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+        document.body.style.backgroundColor = 'goldenrod';
+      });
